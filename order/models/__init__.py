a949f3b48e0bea3019e9dc8038c43c0f9f016984
kelvin <kellzer01@gmail.com>
2026-03-25 10:08:52 -0300

    fix: corrige imports nos arquivos __init__.py - Adiciona OrderItem no order/models/__init__.py - Corrige imports no order/serializers/__init__.py - Mantém __init__.py vazios nos diretórios de testes

--- order/models/__init__.py
+++ order/models/__init__.py
@@ -1,4 +1,4 @@
 from .order import Order
+from .order_item import OrderItem
 
-
-__all__ = ['Order']
+__all__ = ['Order', 'OrderItem']
---
+++ order/serializers/__init__.py
@@ -0,0 +1,3 @@
+from .order_serializers import OrderSerializer, OrderItemSerializer
+
+__all__ = ['OrderSerializer', 'OrderItemSerializer']
