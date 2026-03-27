7440376d31b62860b2cbc9172cdddffd41b267c1
kelvin <kellzer01@gmail.com>
2026-03-27 11:16:51 -0300

    Fix MessageMiddleware for tests

--- settings_test.py
+++ settings_test.py
@@ -1,10 +1,10 @@
-# bookstore/settings_test.py (ou onde estiver seu settings.py)
+# settings_test.py
 from bookstore.settings import *
 
 # Chave secreta para testes
 SECRET_KEY = 'django-insecure-test-key-1234567890!@#$%^&*()'
 
-# Desativa mensagens de segurança para testes
+# Configurações para testes
 DEBUG = True
 
 # Usa SQLite em memória para testes mais rápidos
@@ -15,10 +15,15 @@ DATABASES = {
     }
 }
 
-# Desativa o middleware de mensagens para evitar problemas
-MIDDLEWARE = [m for m in MIDDLEWARE if 'messages' not in m]
+# Garante que o MessageMiddleware está presente
+# Se você modificou o MIDDLEWARE no settings original, adicione-o de volta
+if 'django.contrib.messages.middleware.MessageMiddleware' not in MIDDLEWARE:
+    # Adiciona antes do último middleware (geralmente o SecurityMiddleware)
+    MIDDLEWARE.insert(-1, 'django.contrib.messages.middleware.MessageMiddleware')
 
-# Se tiver autenticação, pode adicionar:
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-]
+# Se você removeu o messages nos INSTALLED_APPS, adicione de volta
+if 'django.contrib.messages' not in INSTALLED_APPS:
+    INSTALLED_APPS.append('django.contrib.messages')
+
+# Configuração de storage para messages
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
