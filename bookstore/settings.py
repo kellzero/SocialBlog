330c8a5f60e84afcd1e6cf3d540861ccd71f212b
kelvin <kellzer01@gmail.com>
2026-03-30 15:44:59 -0300

    removing: unnecesary settings for deploy

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -42,12 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_extensions",
     "rest_framework",
     "rest_framework.authtoken",
     "order",
     "product",
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -59,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
